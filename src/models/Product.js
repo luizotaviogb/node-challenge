08bc611d231bd9b0a1ba3b7149bc209ce9e8b706
luizotaviogoebelbrey <luizotaviogb@gmail.com>
2025-02-28 00:18:22 -0300

    product model rules

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,11 +5,16 @@ const productSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+      minlength: [3, 'Name must be at least 3 characters long'],
+      maxlength: [50, 'Name cannot be longer than 50 characters'],
     },
     code: {
       type: String,
       required: true,
       unique: true,
+      match: /^[a-zA-Z0-9]+$/,
+      minlength: [3, 'Code must be at least 3 characters long'],
+      maxlength: [20, 'Code cannot be longer than 20 characters'],
     },
     value: {
       type: Number,
@@ -19,6 +24,13 @@ const productSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
+    versionKey: 'version',
+    toJSON: {
+      transform: (doc, ret) => {
+        delete ret._id;
+        return ret;
+      },
+    },
   },
 );
 
